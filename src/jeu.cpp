43d6b5462e9559ea4c40c5705c7778be36f98b38
Hedi Amine Chaabani <64554707+chaabollaa@users.noreply.github.com>
2020-05-08 18:44:02 +0200

    Update jeu.cpp

--- src/jeu.cpp
+++ src/jeu.cpp
@@ -10,16 +10,17 @@
 using namespace std;
 jeu::jeu()
 {
-    //zone1 doit etre initialement vide
-    // zone2 doit etre initiaisé aléatoirement
-    verifAction=0;
-    verifPermute=0;
+    
+    
+    verifAction=0;//become 1 if the player make a move
+    verifPermute=0;//become 1 if the player swap cards
     score=0;
 }
 jeu::~jeu(){}
-void jeu::deplacer(int a ,int b)
+void jeu::deplacer(int a ,int b)//move a cards from the top of the stack a to yhe top of the stack b
 {
-    verifAction=0;
+    verifAction=0;//always null when the player try to move a card 
+    //updating the positions a and b to the real positions in the two static vector zone1 and zone2
   int c=0;
   if (a>10)
   {
@@ -28,42 +29,50 @@ void jeu::deplacer(int a ,int b)
    }
    a=a-5;
 
-  if (b<5)
+  if (b<5)//moving a card from zone2 to zone1   
   {
 
       b=b-1;
 
       carte C= zone2[c][a].t.top();
+      /*comparing cards if they have the same type and the card in the zone1 should be also higher rank with only one point
+      for example if rank of the card in zone1 is 2 the rank of the card that the player want to move  be should  3 
+      only in this condition the cards will be moved */
        if (zone1[b].t.top().comparerCarte(zone2[c][a].t.top()))
         {
 
       zone1[b].t.push( zone2[c][a].t.top());
       zone2[c][a].t.pop();
       verifPermute=0;
-      verifAction=1;
-      score+=100;
+      verifAction=1;//the card is moved
+      score+=100;//adding 100 to the score after a good move
+      
 
       }
 
   }
 
-
-if (b>10)
+//moving a card in zone2
+    /*comparing cards if they have the same type and the rank should be inferior for the card the player want to move 
+      for example if rank of the moved card  is 5 the rank of the card on the destination stack should be 6
+      only in this condition the cards will be moved */
+if (b>10)// destination stack is in the second row 
       {
           b=b-11;
 
           carte C=zone2[c][a].t.top();
-           if (zone2[c][a].t.top().comparerCarte(zone2[1][b].t.top()))
+           if (zone2[c][a].t.top().comparerCarte(zone2[1][b].t.top()))//comparing cards 
         {
 
           zone2[1][b].t.push(zone2[c][a].t.top());
           zone2[c][a].t.pop();
           verifPermute=0;
-          verifAction=1;
-          score+=100;
+          verifAction=1;//the card is moved
+          score+=100;//adding 100 to the score after a good move
+          
         }
       }
-if (b<11&&b>4)
+if (b<11&&b>4)//destination stack is in the first row
         {
           b=b-5;
 
@@ -74,12 +83,12 @@ if (b<11&&b>4)
           zone2[0][b].t.push(zone2[c][a].t.top());
           zone2[c][a].t.pop();
           verifPermute=0;
-          verifAction=1;
-          score+=100;
+          verifAction=1;//the card is moved
+          score+=100;//adding 100 to the score after a good move
         }
 
         }
-if (verifAction==0)
+if (verifAction==0)//the player make a bad move 
    {
        cout<<"WARNING:Bad move"<<endl;
        cout<<"this move is not allowed"<<endl;
@@ -100,7 +109,7 @@ void jeu::afficherZone1()
         cout<<"|";
     for(int i=0;i<4;i++)
     {
-        int x=zone1[i].t.top().getRang().size();
+        int x=zone1[i].t.top().getRang().size();//getting the size of the rank so we can print cards with equal space
         if (x==1)
 
         {
@@ -150,7 +159,7 @@ void jeu::afficherZone2()
 
         if (!(zone2[0][j].t.empty()))
         {
-         int x=zone2[0][j].t.top().getRang().size();
+         int x=zone2[0][j].t.top().getRang().size();//getting the size of the rank so we can print cards with equal space
          if (x==1)
 
         {
@@ -197,7 +206,7 @@ void jeu::afficherZone2()
 
         if (!(zone2[1][j].t.empty()))
         {
-         int x=zone2[1][j].t.top().getRang().size();
+         int x=zone2[1][j].t.top().getRang().size();//getting the size of the rank so we can print cards with equal space
          if (x==1)
 
         {
@@ -246,12 +255,13 @@ void jeu::afficherZone2()
 void jeu::distribuer(vector<carte> tableau,int l)
 {
     carte carte1("AS","CO",1),carte14("AS","TR",1),carte27("AS","CA",1),carte40("AS","PI",1);
+    //put 4 AS in zone1 
    zone1[0].t.push(carte14);
    zone1[1].t.push(carte1);
    zone1[2].t.push(carte40);
    zone1[3].t.push(carte27);
       int i(0),j(0),k(0),position,positionS;
-   if (l!=1)
+   if (l!=1)//distribute randomly four cards in each stack 
    {
        while (tableau.size()!=0)
    {
@@ -276,10 +286,10 @@ void jeu::distribuer(vector<carte> tableau,int l)
 
    }
    }
-   if (l==1)
+   if (l==1)//the level 1 so the cards on the top of each stack in zone 2 are choosen from the begining so the player can make moves easily
    {
 
-
+         //the cards with rank 2 are separated from other cards 
         carte carte2("2","CO",2),carte15("2","TR",2),carte28("2","CA",2),carte41("2","PI",2);
         tableau.erase(tableau.begin()+0);
         tableau.erase(tableau.begin()+11);
@@ -291,6 +301,7 @@ void jeu::distribuer(vector<carte> tableau,int l)
         tab.push_back(carte15);
         tab.push_back(carte28);
         tab.push_back(carte41);
+       //8 other cards where each 2 cards have same type and two successive ranks
         for(int m=0;m<4;m++)
         {
 
@@ -310,6 +321,7 @@ void jeu::distribuer(vector<carte> tableau,int l)
             }
 
         }
+       //distrubute randomly 3 cards in each stack in zone 2
        while (tableau.size()!=0)
        {
 
@@ -333,6 +345,7 @@ void jeu::distribuer(vector<carte> tableau,int l)
        i=0;
        j=0;
        k=0;
+       //distribute a card in the top of each stack in zone2
        while (tab.size()!=0)
        {
 
@@ -358,11 +371,12 @@ void jeu::distribuer(vector<carte> tableau,int l)
 
 void jeu::permuterJeu()
 {
-    if (verifPermute==1)
+    if (verifPermute==1)//the player can't swap cards two times without making changes 
         cout<<"you can not have new cards"<<endl;
     else
     {
         verifPermute=1;
+        //swap the two cards in the top of each stack in zone2
         for (int i=0;i<2;i++)
         {
           for(int j=0;j<6;j++)
@@ -377,7 +391,9 @@ void jeu::permuterJeu()
 }
 bool jeu::estBloque()
 {
-    bool test1= true ; // test1 pour vérifier s'il ya encore des déplacements possibles de la zone2 vers la zone1
+    bool test1= true ;
+    /*check if there is a possible move from the zone1 to the zone 2 by comparing cards in the top of  zone2 stacks to the cards 
+    in the the top of zone1 stacks*/
     for (int c=0;c<4;c++)
     {
         if (test1==false)
@@ -399,8 +415,10 @@ bool jeu::estBloque()
         }
     }
     int a(0),b(0),k,l;
-
-    if (test1==true)
+    
+     /*check if there is a possible move in the zone 2 by comparing cards in the top of each  stack in zone2  to the cards 
+    in the the top of zone2  stacks with higher rank*/
+    if (test1==true)//we don't have to check if we have a possible move yet
 
        {
            for (int i=0; i<11;i++)
@@ -437,7 +455,7 @@ bool jeu::estBloque()
 
     return(test1);
 }
-bool jeu::estVideZone2()
+bool jeu::estVideZone2()//verifying if zone2 is empty so the player win and we finish the game
 {
     bool test=true ;
     for(int i=0;i<2;i++)
@@ -456,19 +474,19 @@ bool jeu::estVideZone2()
     return(test);
 }
 
-int jeu::getVerifP()
+int jeu::getVerifP()//getter of verifPermute
 {
     return verifPermute;
 }
-int jeu::getVerifA()
+int jeu::getVerifA()//getter of verifAction
 {
     return verifAction;
 }
-int jeu::getScore()
+int jeu::getScore()//getter of score
 {
     return score;
 }
-void jeu::ajoutScore(long int c)
+void jeu::ajoutScore(long int c)//updating score in the end of game 
 {
     c=c/300;
     int s(1000-c);
